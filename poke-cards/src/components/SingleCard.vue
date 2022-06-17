0c2b88a30bb3e38e092d5ed0ef8e0d0ae0681814
Hargaaya <hargaaya10@gmail.com>
2022-06-17 15:10:39 +0200

    Added type icons

--- poke-cards/src/components/SingleCard.vue
+++ poke-cards/src/components/SingleCard.vue
@@ -5,7 +5,7 @@
         <h3>{{ PokemonInformation.name }}</h3>
         <span>
           <p><b>HP</b> {{ PokemonInformation.stats[0].base_stat }}</p>
-          <i class="fa fa-bolt globe-bg"></i>
+          <i class="fa globe-bg" :class="typeIcon"></i>
         </span>
       </header>
 
@@ -27,7 +27,7 @@
           <ul>
             <li v-for="item in this.moves" v-bind:key="item.id">
               <span>
-                <i class="fa fa-bolt globe-bg"></i>
+                <i class="fa globe-bg" :class="typeIcon"></i>
                 <h3>{{ item.name }}</h3>
                 <b> {{ item.power }}</b>
               </span>
@@ -55,17 +55,20 @@ export default {
     return {
       moves: [],
       movesLoaded: false,
+      typeIcon: "",
     };
   },
   watch: {
     PokemonInformation: function () {
       this.updateMoves();
       this.updateColors();
+      this.updateTypeIcon();
     },
   },
   mounted() {
     this.updateMoves();
     this.updateColors();
+    this.updateTypeIcon();
   },
   methods: {
     updateColors() {
@@ -73,9 +76,10 @@ export default {
         .getColorAsync(this.PokemonInformation.sprites.other.home.front_default)
         .then((color) => {
           document.querySelector(".card-container").style.backgroundColor = color.hex;
-          document.querySelectorAll("fa").forEach((item) => {
-            item.style.color = color.hex;
-          });
+          document.querySelector(".image-container").style.backgroundColor = color.hex;
+          // document.querySelectorAll("fa").forEach((item) => {
+          //   item.style.color = color.hex;
+          // });
         })
         .catch((err) => console.log(err));
     },
@@ -102,31 +106,71 @@ export default {
         });
       }
     },
+
+    updateTypeIcon() {
+      let type = this.PokemonInformation.types[0].type.name;
+      let iconList = {
+        normal: "fa-asterisk bg-indigo-300",
+        fire: "fa-fire bg-yellow-500",
+        water: "fa-water bg-blue-400",
+        grass: "fa-leaf bg-green-500",
+        electric: "fa-bolt bg-yellow-400",
+        ice: "fa-snowflake bg-blue-300",
+        fighting: "fa-hand-fist bg-red-600",
+        poison: "fa-biohazard bg-purple-600",
+        ground: "fa-earth-africa bg-yellow-600",
+        flying: "fa-dove bg-pink-400",
+        psychic: "fa-brain bg-pink-500",
+        bug: "fa-bug bg-green-400",
+        rock: "fa-gem bg-yellow-700",
+        ghost: "fa-ghost bg-purple-400",
+        dark: "fa-moon bg-yellow-900",
+        dragon: "fa-dragon bg-purple-500",
+        steel: "fa-shield bg-gray-400",
+        fairy: "fa-wand-magic-sparkles bg-pink-300",
+      };
+      this.typeIcon = iconList[type];
+    },
   },
 };
 </script>
 
 <style scoped>
 .atropos-container {
+  @apply mx-auto mt-20;
   height: 600px;
   width: 400px;
 }
 
+i {
+  font-size: 14px;
+  text-align: center;
+}
+
+.backgrounds {
+  @apply bg-red-500 bg-gray-400 bg-green-500 bg-yellow-500 bg-pink-300 
+  bg-purple-500 bg-yellow-900 bg-yellow-700 bg-green-400 bg-pink-500
+  bg-pink-400 bg-yellow-600 bg-purple-600 bg-blue-400 bg-blue-300 bg-red-600 
+  bg-indigo-300;
+}
+
 h3 {
-  @apply capitalize font-semibold;
+  @apply capitalize font-semibold m-2 overflow-hidden;
 }
 
 .card-container {
   @apply border-8 border-yellow-400 flex flex-col rounded-2xl
   select-none w-full h-full overflow-hidden;
-  background-image: url("https://www.transparenttextures.com/patterns/clean-gray-paper.png");
-  filter: saturate(1.2);
+  background-image: url(../assets/clean-gray-paper.png), url(../assets/sparkle.webp);
+  background-blend-mode: soft-light;
+  filter: brightness(0.9);
 }
 
-.card-container:hover {
-  background-image: url("https://www.transparenttextures.com/patterns/clean-gray-paper.png"), url(../assets/sparkles.gif);
-  background-blend-mode: color-dodge;
-  filter: contrast(1);
+.card-container:hover,
+.card-container:active {
+  background-image: url(../assets/clean-gray-paper.png), url(../assets/sparkles.gif);
+  background-blend-mode: soft-light;
+  filter: saturation(1.1) brightness(1);
 }
 
 header {
@@ -146,9 +190,10 @@ header p > * {
 }
 
 .image-container {
-  @apply border-4 border-gray-400 mx-auto mt-2 mb-10 flex flex-col justify-center items-center;
-  background-image: url("https://media2.giphy.com/media/TM8Fu4TrgkNSmXh5cw/giphy.gif?cid=ecf05e47ulmw29k70cms7qdcbkgplaao3nqfqlfkaeufuag3&rid=giphy.gif&ct=g");
-  background-size: cover;
+  @apply mx-auto mt-2 mb-8 flex flex-col justify-center items-center shadow-lg;
+  /* background-color: #ffffff; */
+  background-image: url(../assets/trippy-circle.webp), url(../assets/stars.webp);
+  background-blend-mode: overlay;
   background-position: center;
   height: 200px;
   width: 90%;
@@ -160,7 +205,7 @@ header p > * {
 
 .image-container span {
   @apply flex justify-evenly transform skew-x-12 w-80 mt-6 italic text-xs
-  shadow-inner border-2 border-gray-400 bg-gradient-to-t from-gray-400 to-gray-100;
+  shadow-lg border-2 border-gray-400 bg-gradient-to-t from-gray-400 to-gray-100;
 }
 
 .moves-container {
@@ -168,21 +213,34 @@ header p > * {
 }
 
 .moves-container span {
-  @apply flex flex-row justify-between;
+  @apply flex flex-row justify-between mb-2;
 }
 .moves-container p {
-  @apply text-left text-sm pb-1.5;
+  @apply text-left text-sm pb-1.5 mb-3;
 }
 
 .globe-bg {
-  @apply w-7 h-7 p-1.5 rounded-full;
+  @apply w-7 h-7 pt-1.5 rounded-full;
   filter: contrast(4);
   box-shadow: inset -7px -4px 12px rgba(0, 0, 0, 0.3);
   background-image: linear-gradient(-45deg, rgba(255, 255, 220, 0.3) 0%, transparent 100%);
 }
+
+@media screen and (max-width: 420px) {
+  .atropos-container {
+    zoom: 0.9;
+    -moz-transform: scale(0.9);
+  }
+}
+@media screen and (max-width: 380px) {
+  .atropos-container {
+    zoom: 0.8;
+    -moz-transform: scale(0.8);
+  }
+}
 </style>
 
-/** - - Future backgrounds - -
+/** - - Future backgrounds? - -
 https://media2.giphy.com/media/dB66K4Kywc8gPVh6A7/giphy.gif?cid=ecf05e47hbgh7xcagu8hvlaeehpj803bqudtiqhq2wiuwn7h&rid=giphy.gif&ct=g BLUE
 https://media2.giphy.com/media/xT0xeuC4g0tJu17YSA/giphy.gif?cid=ecf05e47sgv6ibmwve4jlllysublpri5n8qtcurnll8ivw6f&rid=giphy.gif&ct=g YELLOW
 https://media4.giphy.com/media/YqhIK6Gbor6CLeloBq/giphy.gif?cid=ecf05e47hkdtil2q088l3g8lvp5zk43htantb6t76s7w7m4u&rid=giphy.gif&ct=g PURPLE
